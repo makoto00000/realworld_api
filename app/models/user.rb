a4b80ec0560df4bf75c302d3990ad604d7d0d7fd
makoto hata <makotohata0429@gmail.com>
2023-12-28 22:06:43 +0900

    generate article model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :articles
   has_secure_password
   validates :username, :email, :password, presence: true
 end
---
+++ db/migrate/20231228125817_create_articles.rb
@@ -0,0 +1,14 @@
+class CreateArticles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :slug, null: false
+      t.string :title, null: false
+      t.string :description
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :articles, :slug, unique: true
+  end
+end
